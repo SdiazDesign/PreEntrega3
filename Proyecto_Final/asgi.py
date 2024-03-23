05691c6cea5fcf2df0c6d67fcd3999be8ae3b845
Sebastian Diaz <sdiaz@sidom.io>
2024-03-23 17:01:28 -0300

    agrego aboutme

--- Proyecto_Final/asgi.py
+++ Proyecto_Final/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for PreEntrega3 project.
+ASGI config for Proyecto_Final project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PreEntrega3.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Proyecto_Final.settings')
 
 application = get_asgi_application()
--- Proyecto_Final/urls.py
+++ Proyecto_Final/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for PreEntrega3 project.
+URL configuration for Proyecto_Final project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
